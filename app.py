d36c760fabd546ab36460a8a5904074ecf5b341d
Somit <74805178+somitsebastian007@users.noreply.github.com>
2025-03-26 20:52:27 +0530

    added welcome message

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 def greet(name):
-    return f"Hello, {name}!"
+    return f"Hello, {name}! Welcome to DevOps Training"
 
 if __name__ == "__main__":
     user_name = input("Enter your name: ")
